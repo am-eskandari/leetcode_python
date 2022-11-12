4e188502bff5cc19ceddb223018ebc1957c7263a
Amir Eskandari <113070116+am-eskandari@users.noreply.github.com>
2022-11-12 09:07:21 -0800

    fix typo in anagram.py

--- leetcode242_validanagram/anagram.py
+++ leetcode242_validanagram/anagram.py
@@ -7,23 +7,23 @@ https://www.youtube.com/watch?v=9UtInBqnCgA
 
 def anagram(string_one, string_two):
     """
-    Determine if two given strings are an leetcode242_validanagram; return True if they are an leetcode242_validanagram, else False.
+    Determine if two given strings are an anagram; return True if they are an anagram, else False.
 
     :param string_one: a string of a single word
     :param string_two: a string of a single word
     :precondition: string_one and string_two has to be a string containing single word
     :precondition: string_one and string_two has to be a string of the same length
-    :postcondition: correctly determine if the strings are an leetcode242_validanagram
-    :return: True if string_one and string_two are an leetcode242_validanagram, else False
+    :postcondition: correctly determine if the strings are an anagram
+    :return: True if string_one and string_two are an anagram, else False
 
-    >>> test_string_one = "leetcode242_validanagram"
+    >>> test_string_one = "anagram"
     >>> test_string_two = "nagaram"
-    >>> leetcode242_validanagram(test_string_one, test_string_two)
+    >>> anagram(test_string_one, test_string_two)
     True
 
     >>> test_string_one = "rat"
     >>> test_string_two = "car"
-    >>> leetcode242_validanagram(test_string_one, test_string_two)
+    >>> anagram(test_string_one, test_string_two)
     False
     """
     return sorted(list(string_one.lower())) == sorted(list(string_two.lower()))
@@ -40,4 +40,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
