af869158c6cae4f8d0b8c26e04b4566866b7323e
Amir Eskandari <113070116+am-eskandari@users.noreply.github.com>
2022-12-10 14:03:13 -0800

    Update maximum_subarray.py

--- leetcode_medium/leetcode53_maximumsubarray/maximum_subarray.py
+++ leetcode_medium/leetcode53_maximumsubarray/maximum_subarray.py
@@ -7,24 +7,45 @@ https://www.youtube.com/watch?v=5WZl3MMT0Eg
 https://leetcode.com/problems/two-sum/
 """
 
-# Given an integer array nums, find the
-# subarray
-# which has the largest sum and return its sum.
 
-#  
+def maximum_subarray(nums: list) -> int:
+    """
+    Given an integer array nums, find the subarray which has the largest sum and return its sum.
 
-# Example 1:
+    :param nums: list of integers
+    :precondition: nums is a list of integers
+    :postcondition: return the subarray which has the largest sum and return its sum
+    :return: integer
 
-# Input: nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
-# Output: 6
-# Explanation: [4, -1, 2, 1] has the largest sum = 6.
+    >>> maximum_subarray([-2, 1, -3, 4, -1, 2, 1, -5, 4])
+    6
 
-# Example 2:
+    >>> maximum_subarray([1])
+    1
 
-# Input: nums = [1]
-# Output: 1
+    >>> maximum_subarray([5, 4, -1, 7, 8])
+    23
+    """
+    # create a variable to store the current sum
+    current_sum = 0
 
-# Example 3:
+    # create a variable to store the maximum sum
+    maximum_sum = 0
 
-# Input: nums = [5, 4, -1, 7, 8]
-# Output: 23
+    # loop through nums
+    for i in range(len(nums)):
+        # if the current sum is less than 0
+        if current_sum < 0:
+            # set the current sum to 0
+            current_sum = 0
+
+        # add nums[i] to the current sum
+        current_sum += nums[i]
+
+        # if the current sum is greater than the maximum sum
+        if current_sum > maximum_sum:
+            # set the maximum sum to the current sum
+            maximum_sum = current_sum
+
+    # return the maximum sum
+    return maximum_sum
